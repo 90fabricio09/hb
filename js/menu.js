dac96e717019cda620b1d9a95d104b16b16e9b13
90fabricio09 <90934129+90fabricio09@users.noreply.github.com>
2024-04-02 19:59:49 -0300

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,32 @@
 var btMenu = document.getElementById("menu-mobile");
 var menu = document.getElementById("menu");
+var links = document.querySelectorAll(".menu__link");
+var icon = document.querySelector("#menu-mobile i");
 
-btMenu.addEventListener('click', function(e){console.log(menu.style.display);
+btMenu.addEventListener('click', function(e){
   if (menu.style.display === 'flex') {
     menu.style.display = 'none';
-  }else{
+    icon.classList.remove('bi-x'); // remove a classe 'bi-x' quando o menu estiver fechado
+    icon.classList.add('bi-list'); // adiciona a classe 'bi-list' quando o menu estiver fechado
+    icon.style.color = '#323232';
+  } else {
     menu.style.display = 'flex';
+    icon.classList.remove('bi-list'); // remove a classe 'bi-list' quando o menu estiver aberto
+    icon.classList.add('bi-x'); // adiciona a classe 'bi-x' quando o menu estiver aberto
+    icon.style.color = 'white';
   }
   e.preventDefault();
 });
+
+var links = document.querySelectorAll(".menu__link");
+
+for (var i = 0; i < links.length; i++) {
+  links[i].addEventListener('click', function(e) {
+    if (window.innerWidth <= 640) {
+      menu.style.display = 'none';
+      icon.classList.remove('bi-x'); // remove a classe 'bi-x' quando o menu estiver fechado
+      icon.classList.add('bi-list'); // adiciona a classe 'bi-list' quando o menu estiver fechado
+      icon.style.color = '#323232';
+    }
+  });
+}
