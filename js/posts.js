0b0fe86a40f74ce23b582b409d939cedce402edc
90fabricio09 <90934129+90fabricio09@users.noreply.github.com>
2024-04-02 19:53:50 -0300

    Add files via upload

--- js/posts.js
+++ js/posts.js
@@ -1,26 +1,26 @@
 var posts = [
   {
-    "imagem": "../img/transporte_pessoas.png",
+    "imagem": "https://media.discordapp.net/attachments/973694599547744276/1202456117683159050/transporte_corporativo.png?ex=65cd8587&is=65bb1087&hm=53b02c2f39935f34ebebf670e8fc0fc078cd0204bf31fde581c5043ff03b8744&=&format=webp&quality=lossless&width=627&height=353",
     "titulo": "TRANSPORTE DE PESSOAS",
     "description": "A HB Fretamento é uma empresa renomada no setor de transporte de passageiros. Com uma frota moderna e uma equipe de profissionais altamente qualificados"
   },
   {
-    "imagem": "../img/aluguel_van.png",
+    "imagem": "https://media.discordapp.net/attachments/973694599547744276/1202452686730174484/aluguel_van.png?ex=65cd8255&is=65bb0d55&hm=bb5b705accc13065cabd35f6d17abcc3aaac0ee17fd7c5abd9564ae326b2f3f0&=&format=webp&quality=lossless&width=627&height=353",
     "titulo": "ALUGUEL DE VANS",
     "description": "A HB Fretamento é uma empresa líder no mercado de aluguel de vans, oferecendo soluções de transporte personalizadas para atender a uma variedade de necessidades. Seja para viagens corporativas, eventos especiais ou transporte diário."
   },  
   {
-    "imagem": "../img/transporte_executivo.png",
+    "imagem": "https://media.discordapp.net/attachments/973694599547744276/1202454242569363496/transporte_executivo.png?ex=65cd83c7&is=65bb0ec7&hm=e658fa73c0f5d2f8e725f44b3b1de579f3445a83b4a814f993b7030088a2fef2&=&format=webp&quality=lossless&width=627&height=353",
     "titulo": "TRANSPORTE EXECUTIVO",
     "description": "A HB Fretamento é uma referência no mercado de transporte executivo, proporcionando um serviço de alta qualidade para profissionais e empresas. Com uma frota de veículos modernos, a HB Fretamento garante um transporte executivo confortável, seguro e pontual."
   },
   {
-    "imagem": "../img/cargas.png",
+    "imagem": "https://media.discordapp.net/attachments/973694599547744276/1202462682897326151/cargas.png?ex=65cd8ba4&is=65bb16a4&hm=4683a37ecabc9939b11baa6d69883327189b8852c0a37331c3408fe2d54cc445&=&format=webp&quality=lossless&width=627&height=353",
     "titulo": "TRANSPORTE DE CARGAS",
     "description": "A HB Fretamento é uma empresa de confiança no setor de transporte de cargas, oferecendo soluções personalizadas para atender a todas as suas necessidades de logística.O serviço de transporte de cargas da HB é conhecido por sua confiabilidade e pontualidade."
   },
   {
-    "imagem": "../img/mudanca.png",
+    "imagem": "https://media.discordapp.net/attachments/973694599547744276/1202462676308070430/mudanca.png?ex=65cd8ba2&is=65bb16a2&hm=d31421bba49795e5eb904a206bffaea73bff0231650393ba87eef078364d7800&=&format=webp&quality=lossless&width=627&height=353",
     "titulo": "MUDANÇAS",
     "description": "A HB Fretamento é uma empresa de destaque no setor de mudanças, oferecendo um serviço completo para facilitar o processo de mudança. Com uma frota de veículos adequados, a HB garante que suas pertences sejam transportadas com segurança e eficiência."
   }
